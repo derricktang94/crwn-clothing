c84a9dd153e61390308c8ee2d3460b7203c1e21d
Derrick Tang <derrick_tang@tech.gov.sg>
2020-09-18 17:09:03 +0800

    added redux, and implemented userReducer and userActions to header and app components

---
+++ src/redux/user/user-reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+// state is something redux store will going to pass to reducer whenever action is fire
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
